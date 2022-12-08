147fdb2b3df03d9624c733f26616cdd17ddc50ac
Edward Chang <edchang23@berkeley.edu>
2022-12-07 16:57:36 -0800

    Added user input prototype to src/move_arm/src/main.py

--- src/move_arm/src/main.py
+++ src/move_arm/src/main.py
@@ -21,30 +21,85 @@ from motion_function import MotionFunction
 
 
 def main():
-    
+
     # Create a MotionFunction object
     motion_function_object = MotionFunction()
-    
-    # Get the glass position 
+
+    # Get the glass position
     glass_1_position = np.array([.5, .5, .5]) # get the transformation from AR_tag
     glass_2_position = np.array([.25, .25, .25]) # get the transformation from AR_tag
     container_position = np.array([1, 1, 1]) # get the container position
-    
+
+    """
+    TEMPORARY """
+    """ USER INPUT ORDER """
+
+    ingredients_list = [glass_1_position, glass_2_position]
+
+    print('|===============================|')
+    print('| ** Make-To-Order Cocktails ** |')
+    print('| Mixers:                       |')
+    print('| 1. Cup_1                      |')
+    print('| 2. Cup_2                      |')
+    print('|===============================|')
+    print('Input ingredient 1:')
+    order_1 = eval(input()) - 1
+    print('Input ingredient 2 or 0 for none:')
+    order_2 = eval(input()) - 1
+
+    motion_function_object.going_and_take_glass(ingredients_list[order_1])
+
+    motion_function_object.going_to_container_and_pouring(container_position)
+
+    motion_function_object.putting_glass_back(ingredients_list[order_1])
+
+    # Glass 2
+    if order_2 > 0:
+        motion_function_object.going_and_take_glass(ingredients_list[order_2])
+
+        motion_function_object.going_to_container_and_pouring(container_position)
+
+        motion_function_object.putting_glass_back(ingredients_list[order_2])
+
+    """
+    TEMPORARY """
+
+
+    """
+    Actions (possibly easier)
+    - move(ingredient_1_position)
+    - close_gripper()
+    - move(target_cup_position)
+    - rotate_gripper()
+    - move(ingredient_1_position)
+    - open_gripper()
+
+    - move(default_position)
+
+    - move(ingredient_2_position)
+    - close_gripper()
+    - move(target_cup_position)
+    - rotate_gripper()
+    - move(ingredient_2_position)
+    - open_gripper()
+
+    -move(default_position)
+    """
     # Glass 1
     motion_function_object.going_and_take_glass(glass_1_position)
 
     motion_function_object.going_to_container_and_pouring(container_position)
-    
+
     motion_function_object.putting_glass_back(glass_1_position)
-    
+
     # Glass 2
     motion_function_object.going_and_take_glass(glass_2_position)
 
     motion_function_object.going_to_container_and_pouring(container_position)
-    
+
     motion_function_object.putting_glass_back(glass_2_position)
-    
-    
+
+
 
 
 
